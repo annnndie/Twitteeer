87b1dafa3ffa0af2d2fdeef68470360edac8708b
yuchan <lumir1031@gmail.com>
2020-06-27 01:02:32 +0800

    三欄畫面

--- app/javascript/styles/index.js
+++ app/javascript/styles/index.js
@@ -1,5 +1,9 @@
 import './reset.scss'
 import './navbar.scss'
+import './tweet.scss'
+import './left-nav.scss'
+import './main.scss'
+import './friend-nav.scss'
 import 'bootstrap/dist/css/bootstrap.css'
 import '@fortawesome/fontawesome-free/js/all';
 import '@fortawesome/fontawesome-free/css/all.min.css';
