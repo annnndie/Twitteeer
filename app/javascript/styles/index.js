0a673d9228d59cdef624fde81d208bb14cd9252b
yuchan <lumir1031@gmail.com>
2020-06-26 18:21:45 +0800

    install bootstrap and fontawesome

---
+++ app/javascript/styles/index.js
@@ -0,0 +1,5 @@
+import './reset.scss'
+import './navbar.scss'
+import 'bootstrap/dist/css/bootstrap.css'
+import '@fortawesome/fontawesome-free/js/all';
+import '@fortawesome/fontawesome-free/css/all.min.css';
