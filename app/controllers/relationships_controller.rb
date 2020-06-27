1d6e6baf942d654f9a76127e0c01e214a347da7b
yuchan <lumir1031@gmail.com>
2020-06-27 18:43:22 +0800

    user 可以 follow 其他 user

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -3,15 +3,14 @@ class RelationshipsController < ApplicationController
   end
 
   def create
-    followed_user = User.find_by(id: params[:user_id])
-    relationship = current_user.active_relationship.build(followed_id: followed_user.id)
+    relationship = current_user.active_relationships.build(followed_id: params[:id])
 
     if relationship.save
       redirect_to root_path, notice: '好友增加成功'
     else
       redirect_to root_path, notice: '好友增加失敗'
     end
-    
+
   end
 
 end
