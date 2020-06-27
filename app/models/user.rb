899051481cc04b9d9020e32512f569d44177f175
yuchan <lumir1031@gmail.com>
2020-06-27 15:33:51 +0800

    set model

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,5 +12,9 @@ class User < ApplicationRecord
 
   # relation
   has_many :tweets
+  has_many :active_relationships, class_name: "Relationship", foreign_key:"follower_id", dependent: :destroy
+  has_many :passive_relationships, class_name: "Relationship", foreign_key:"followed_id", dependent: :destroy
+  has_many :following_users, through: :active_relationships, source: :followed_user
+  has_many :followers, through: :passive_relationships, source: :follower_user
 
 end
