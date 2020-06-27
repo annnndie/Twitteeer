ef67aca23430e13a8ec8c79863e1504a3f50f94b
yuchan <lumir1031@gmail.com>
2020-06-27 14:24:01 +0800

    亂數找使用者

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :who_can_follow
+
+  def who_can_follow
+    @can_follow_user = User.where('id != ?', current_user.id).sample(3)
+  end
 
   private
   def configure_permitted_parameters
